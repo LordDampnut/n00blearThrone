9514c204379705e66d2201e7ee76a364c58acb67
LordDampnut <light2000@hotmail.de>
2021-01-21 23:37:36 +0100

    Initial commit

---
+++ NTCONST.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 21 18:12:53 2021
+
+@author: 4Null
+"""
+import numpy
+
+enemies = numpy.loadtxt("enemies.txt", delimiter="\t", dtype=str)
+world = numpy.loadtxt("worlds.txt", delimiter="\t", dtype=str)
+crown = numpy.loadtxt("crowns.txt", delimiter="\t", dtype=str)
+guns = numpy.loadtxt("guns.txt", delimiter="\t", dtype=str)
+mutations = numpy.loadtxt("mutations.txt", delimiter="\t", dtype=str)
+#level
+#health
+#kills
+#loops
+#skin (0,1)
+#ultra
+#
+
+
+character = ["","Fish", "Crystal", "Eyes", "Melting", "Plant", "Y.V.", "Steroids",
+       "Robot", "Chicken", "Rebel", "Horror", "Rogue", "Skeleton", "Frog"]
+
+i = 0
+
+def getCharacter(n):
+    return character[n]
+
+def getLastHitEnemy(n):
+    return enemies[n+1][1]
+
+def getWorld(n):
+    i = numpy.where(world == str(n))
+    #i = world[0].index(str(n))
+    return world[i][0]
+
+def getCrown(n):
+    return crown[n+1][1]
+
+def getGuns(n):
+    return guns[n][1]
---
+++ die.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 21 18:05:18 2021
+
+@author: 4Null
+"""
+import NTCONST
+import json
+import requests
+import numpy
+
+def recordRun(): #write previous run to file, csv format
+    
+    time = numpy.loadtxt("output/history.txt", delimiter=",", dtype=str)
+    if not (previousRun["timestamp"] == int(time.T[0][-1])):      
+        file = open("output/history.txt","a")
+        file.write(
+                "\n" + 
+                str(previousRun["timestamp"]) + "," +
+                str(NTCONST.getCharacter(previousRun["char"])) + "," +
+                str(previousRun["charlvl"]) + "," +
+                str(previousRun["crown"]) + "," + 
+                str(previousRun["health"]) + "," +
+                str(previousRun["kills"]) + "," +
+                str(previousRun["lasthit"]) + "," +
+                str(previousRun["level"]) + "," +
+                str(previousRun["loops"]) + "," +
+                str(previousRun["mutations"]) + "," +
+                str(previousRun["skin"]) + "," +
+                str(previousRun["type"]) + "," +
+                str(previousRun["ultra"]) + "," +
+                str(previousRun["wepA"]) + "," +
+                str(previousRun["wepB"]) + "," +
+                str(previousRun["win"]) + "," +
+                str(previousRun["world"])            
+                )
+        #timestamp,char,charlvl,crown,health,kills,lasthit,level,loops,mutations,
+        #skin,type,ultra,wepA,wepB,win,world
+        file.close()
+
+r = requests.get('https://tb-api.xyz/stream/get?s=76561198041682918&key=BFGJLMYZ6')
+jsonFormat = r.json()
+
+#print(jsonFormat["charlvl"])
+
+currentRun = jsonFormat["current"]
+previousRun = jsonFormat["previous"]
+
+if  bool (currentRun):
+    key = "current"
+    #print("Hello World")
+elif bool (previousRun):
+    key = "previous"
+    #print("Hah noob")
+else:
+    #print("No current or previous run detected!")
+    exit()  
+
+print("World:\t\t" + NTCONST.getWorld(jsonFormat[key]["world"]) + " - " + 
+      str(jsonFormat[key]["level"]))
+print("Chacacter:\t" + NTCONST.getCharacter(jsonFormat[key]["char"]) +
+  " Lv: " + str(jsonFormat[key]["charlvl"]))
+
+if jsonFormat[key]["health"] == 0:
+    print("Killed by:\t" + str(NTCONST.getLastHitEnemy(jsonFormat[key]["lasthit"])))
+    recordRun() #hah you died
+else:
+    print("Last hit:\t" + NTCONST.getLastHitEnemy(jsonFormat[key]["lasthit"]))
+    
+print("Kills:\t\t" + str(jsonFormat[key]["kills"]))
