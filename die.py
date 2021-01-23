8f7792addf7f2b8cb93c3830884f6bf96eaca86a
ThomSab <jaspervogel97@gmail.com>
2021-01-23 19:27:07 +0100

    implemented mainloop and more butchery

--- die.py
+++ die.py
@@ -6,6 +6,7 @@ Created on Thu Jan 21 18:05:18 2021
 """
 import os
 import json
+import time
 import NTCONST
 import requests
 import numpy as np
@@ -18,15 +19,14 @@ def API_get():
 	jsonFormat = requests.get(NTCONST.getStreamlink()).json()
 	return jsonFormat,jsonFormat["current"],jsonFormat["previous"]
 
-def UpdateRuns():
+def update_runs(jsonFormat,currentRun,previousRun):
 	"""
-	Input: None
+	Input: jsonFormat,currentRun,previousRun --> the API outputs
 	Output: None
 	Saves the runs currently diplayed by the nt API to the json file.
 	If the API displays a run that has previously been saved it overwrites it with the more recent version of the run.
 	Individual runs are identified by their timestamps.
 	"""
-	jsonFormat,currentRun,previousRun = API_get()
 	
     with open("output/history.json","a") as jsonfile:
 		history_dict = json.load(jsonfile)
@@ -37,34 +37,45 @@ def UpdateRuns():
 			history_dict[str(currentRun("timestamp"))] = {variable : str(currentRun[_]) for _ in variables}
 		json.dump(history_dict,jsonfile)
 
-jsonFormat,currentRun,previousRun = API_get()
+def continuous_update(interval):
+	"""
+	Input: interval (interval between updates)
+	Output: None
+	Fetches the current API output saves it to history.json
+	and puts the API output to the console 
+	"""
+	current_run = None
+	loop_condition = True
+		while loop_condition:
+			time.sleep(interval) #aktualisiert alle {interval} sekunden
+			
+			jsonFormat,currentRun,previousRun = API_get() #gets current API output
+			update_runs(jsonFormat,currentRun,previousRun) #history.json update
+			
+			if not bool (currentRun) and not bool (previousRun):
+				print("No current or previous run detected!")
+				loop_condition=False
+				
+			key="current"
+			if not bool(currentRun):
+				key="previous"
+			
+			print("World:\t\t" + NTCONST.getWorld(jsonFormat[key]["world"]) + " - " + str(jsonFormat[key]["level"]))
+			print("Chacacter:\t" + NTCONST.getCharacter(jsonFormat[key]["char"]) + " Lv: " + str(jsonFormat[key]["charlvl"]))
+			print("Weapons:\t" + NTCONST.getGuns(jsonFormat[key]["wepA"]) + " - " + NTCONST.getGuns(jsonFormat[key]]["wepB"]))
+			print("Crown:\t\t" + NTCONST.getCrown(jsonFormat[key]["crown"]))
+			print("Loop:\t\tL" + str(jsonFormat[key]["loops"]))
+			print("Kills:\t\t" + str(jsonFormat[key]["kills"]))
 
-if  bool (currentRun):
-    key = "current"
-    #print("Hello World")
-elif bool (previousRun):
-    key = "previous"
-    #print("Hah noob")
-else:
-    #print("No current or previous run detected!")
-    exit()  
+			if current_run_ts != currentRun("timestamp") and currentRun != None:
+				print("Killed by:\t" + str(NTCONST.getLastHitEnemy(jsonFormat["previous"]["lasthit"])))
+				current_run_ts = currentRun("timestamp")
+			else:
+				print("Last hit:\t" + NTCONST.getLastHitEnemy(jsonFormat[key]["lasthit"]))
+			
+if __name___ == "__main___": 
+#macht dass das folgende nur ausgefuehrt wird wenn die.py direkt ausgefuehrt wird
+#so kann man z.B. API_get woanders importieren
 
-print("World:\t\t" + NTCONST.getWorld(jsonFormat[key]["world"]) + " - " + 
-      str(jsonFormat[key]["level"]))
-print("Chacacter:\t" + NTCONST.getCharacter(jsonFormat[key]["char"]) +
-  " Lv: " + str(jsonFormat[key]["charlvl"]))
+	continuous_update(5) #update every five seconds
 
-print("Weapons:\t" + NTCONST.getGuns(jsonFormat[key]["wepA"]) + " - " + 
-      NTCONST.getGuns(jsonFormat[key]["wepB"]))
-
-print("Crown:\t\t" + NTCONST.getCrown(jsonFormat[key]["crown"]))
-
-print("Loop:\t\tL" + str(jsonFormat[key]["loops"]))
-
-if jsonFormat[key]["health"] == 0:
-    print("Killed by:\t" + str(NTCONST.getLastHitEnemy(jsonFormat[key]["lasthit"])))
-    recordRun() #hah you died
-else:
-    print("Last hit:\t" + NTCONST.getLastHitEnemy(jsonFormat[key]["lasthit"]))
-    
-print("Kills:\t\t" + str(jsonFormat[key]["kills"]))
