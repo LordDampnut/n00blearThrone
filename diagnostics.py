7c27bb5f5c79f167f47ee9a68b26eb3bd5f7cbef
ThomSab <jaspervogel97@gmail.com>
2021-01-25 16:12:22 +0100

    added diagnostics file

---
+++ diagnostics.py
@@ -0,0 +1,16 @@
+import numpy
+import die #lol same bro
+
+
+def get_history():
+    """
+    loads the history.json file and returns it as a dictionary
+    """
+    with open("output/history.json","r") as jsonfile:
+        return json.load(jsonfile)
+
+
+def 
+
+if __name__ == "__main__":
+    history_dict = get_history()
