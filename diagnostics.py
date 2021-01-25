9dabf253d7faf855279f0b2ff21f4335c0a346dc
ThomSab <jaspervogel97@gmail.com>
2021-01-25 16:26:49 +0100

    added basic structure of diagnostics

--- diagnostics.py
+++ diagnostics.py
@@ -1,8 +1,8 @@
 import numpy
 import die #lol same bro
+history_dict={}
 
-
-def get_history():
+def get_history_dict():
     """
     loads the history.json file and returns it as a dictionary
     """
@@ -10,7 +10,31 @@ def get_history():
         return json.load(jsonfile)
 
 
-def 
+def get_average_kills(history_dict=history_dict):
+    average_kills = False #TODO calculate from history dictionary
+    return average_kills
+
+def get_death_probabilities(history_dict=history_dict)
+    death_probabilities = False #TODO see above
+    return death_probabilities
+    
+def get_from_history(*args)
+    """
+    Input: 
+        Keywords for the required values eg. average_kills, death_probabilities etc.
+    Output:
+        The calculated values for the requested input arguments
+    """
+    
+    argument_dict={"average_kills":get_average_kills, "death_probabilities":get_death_probabilities}
+    
+    for argument in args: #this stops the code and throws an error but it could be changed to warnings instead
+        assert (argument in argument_dict.keys()), "Requested argument cannot be fetched from api"
+    
+    return [argument_dict[argument]() for argument in args] #calls the get_ function for each argument
+
+
+
 
 if __name__ == "__main__":
     history_dict = get_history()
